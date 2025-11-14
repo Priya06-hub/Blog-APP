1c115c9cbbf13f6c0e6fe8f19f8f072b2218a9d8
Priya06-hub <prajapatipriya079@gmail.com>
2025-11-14 13:19:31 -0500

    change url of the logout

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/' 
+LOGIN_REDIRECT_URL = '/'
 
-# URL to redirect to after a user logs out (defaults to rendering 'registration/logged_out.html')
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/login/'
